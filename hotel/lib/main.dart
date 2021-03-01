4d27a27189ac5d08d02316a8a90545f0a84faf48
Andrew <Shankapotamus66@users.noreply.github.com>
2021-03-01 12:26:39 -0800

    Buttons and Hotel List View added

--- hotel/lib/main.dart
+++ hotel/lib/main.dart
@@ -1,15 +1,23 @@
 import 'dart:math';
+import 'package:flutter/material.dart';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //await Firebase.initializeApp();
   runApp(MyApp());
-
+  /*
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+  */
 }
 
 class MyApp extends StatelessWidget {
@@ -20,6 +28,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        brightness: Brightness.dark,
+        backgroundColor: Colors.blueGrey.shade100,
+        primaryColor: Color(0xffbb86fc),
+        accentColor: Color(0xffbb86fc),
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -29,7 +41,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        buttonColor: Color(0xffbb86fc),
+        //primaryColor: Colors.black54,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
@@ -43,7 +57,7 @@ class MyApp extends StatelessWidget {
             print("You have an error! ${snapshot.error.toString()}");
             return Text("Something went wrong!");
           } else if (snapshot.hasData) {
-            return MyHomePage(title: "My Amazing App!");
+            return MyHomePage(title: "MAD Hotel");
           } else {
             return Center(
               child: CircularProgressIndicator(),
@@ -79,6 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+
     DatabaseReference _testRef = FirebaseDatabase.instance.reference().child("test");
     _testRef.set("Hello World ${Random().nextInt(100)}");
 
@@ -109,38 +124,200 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Container(
+          //margin: const EdgeInsets.all(100.0),
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 40,
+                    width: 225,
+                    child: RaisedButton(
+                      textColor: Color(0xFF303030),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HotelList()),
+                        );
+                      },
+                      child: const Text('List Hotels', style: TextStyle(fontSize: 20)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: SizedBox(
+                  height: 40,
+                  width: 225,
+                  child: RaisedButton(
+                    textColor: Color(0xFF303030),
+                    onPressed: () {},
+                    child: const Text('Create Booking', style: TextStyle(fontSize: 20)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: SizedBox(
+                  height: 40,
+                  width: 225,
+                  child: RaisedButton(
+                    textColor: Color(0xFF303030),
+                    onPressed: () {},
+                    child: const Text('Update Booking', style: TextStyle(fontSize: 20)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: SizedBox(
+                  height: 40,
+                  width: 225,
+                  child: RaisedButton(
+                    textColor: Color(0xFF303030),
+                    onPressed: () {},
+                    child: const Text('Delete Booking', style: TextStyle(fontSize: 20)),
+                  ),
+                ),
+              ),
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
         ),
+
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HotelList()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+class HotelList extends StatelessWidget {
+  final dbRef = FirebaseDatabase.instance.reference().child("Hotels");
+  List<Map<dynamic, dynamic>> lists = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hotel List"),
+      ),
+      body: Center(
+        child: Container(
+        margin: const EdgeInsets.all(10.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                //final dbRef = FirebaseDatabase.instance.reference().child("pets");
+                FutureBuilder(
+                    future: dbRef.once(),
+                    builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+                      if (snapshot.hasData) {
+                        lists.clear();
+                        Map<dynamic, dynamic> values = snapshot.data.value;
+                        values.forEach((key, values) {
+                          lists.add(values);
+                        });
+                        return new ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: lists.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Card(
+                                child: InkWell(
+                                  splashColor: Colors.purple.withAlpha(30),
+                                  onTap: () {
+                                    //print('Card tapped.');
+                                  },
+                                  child: Container(
+                                    height: 100,
+                                    width: double.infinity,
+                                    child: Padding(
+                                      padding: EdgeInsets.all(10.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Text("Hotel: " + lists[index]["hName"]),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            });
+                      }
+                      return CircularProgressIndicator();
+                    }),
+                Card(
+                  //height: 100,
+                  child: InkWell(
+                    splashColor: Colors.purple.withAlpha(30),
+                    onTap: () {
+                      //print('Card tapped.');
+                    },
+                    child: Container(
+                      height: 100,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Go back!'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+          ),
+        ),
+      ),
     );
   }
 }
